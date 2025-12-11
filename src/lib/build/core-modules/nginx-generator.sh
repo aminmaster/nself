3f0408c1bddb2466c80d9382fe2acecdc6c5e500
Amin Master <aminmaster@equilibria.ngo>
2025-12-10 19:20:47 -0600

    fix: set Host header to localhost for MLflow to prevent 403

--- src/lib/build/core-modules/nginx-generator.sh
+++ src/lib/build/core-modules/nginx-generator.sh
@@ -459,7 +459,7 @@ server {
     location / {
         proxy_pass http://mlflow:5005;
         proxy_http_version 1.1;
-        proxy_set_header Host \$host;
+        proxy_set_header Host localhost;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
