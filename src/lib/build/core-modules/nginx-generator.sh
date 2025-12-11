114aea2305176ad5c0c14fa4bd43426fa30b925e
Amin Master <aminmaster@equilibria.ngo>
2025-12-10 18:46:15 -0600

    fix: update MLflow internal port to 5005

--- src/lib/build/core-modules/nginx-generator.sh
+++ src/lib/build/core-modules/nginx-generator.sh
@@ -457,7 +457,7 @@ server {
     ssl_certificate_key /etc/nginx/ssl/${base_domain}/privkey.pem;
 
     location / {
-        proxy_pass http://mlflow:5000;
+        proxy_pass http://mlflow:5005;
         proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
