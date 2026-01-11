a7abb43220030e17b9b1f6f69032079a68594ddb
Amin Master <aminmaster@equilibria.ngo>
2026-01-10 20:41:46 -0600

    fix(flowise): remove hardcoded auth env vars to fix login ui

--- src/services/docker/compose-modules/fw.sh
+++ src/services/docker/compose-modules/fw.sh
@@ -36,8 +36,6 @@ generate_fw_stack() {
       DATABASE_USER: postgres
       DATABASE_PASSWORD: ${NSELF_ADMIN_PASSWORD:-${POSTGRES_PASSWORD:-aiopassword}}
       DATABASE_NAME: flowise
-      FLOWISE_USERNAME: ${FLOWISE_USERNAME:-admin@equilibria.org}
-      FLOWISE_PASSWORD: ${NSELF_ADMIN_PASSWORD:-${POSTGRES_PASSWORD:-aiopassword}}
       # Use password auth
       FLOWISE_AUTH_TYPE: password
       FLOWISE_SECRET_KEY: ${FLOWISE_SECRET_KEY:-flowise_secret_key_change_me}
