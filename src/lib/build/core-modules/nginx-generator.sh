807580531ec4bcf59855d12836ddf8ac2c5eb0b3
Amin Master <aminmaster@equilibria.ngo>
2026-01-15 10:23:58 -0600

    fix(nginx): standardise escaping in ragflow nginx block to fix invalid variable error

--- src/lib/build/core-modules/nginx-generator.sh
+++ src/lib/build/core-modules/nginx-generator.sh
@@ -845,14 +845,14 @@ server {
     }
 
     location / {
-        set \\\$target_rf rf-ragflow;
-        proxy_pass http://\\\$target_rf:80;
+        set \$target_rf rf-ragflow;
+        proxy_pass http://\$target_rf:80;
         proxy_http_version 1.1;
-        proxy_set_header Host \\\$host;
-        proxy_set_header X-Real-IP \\\$remote_addr;
-        proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \\\$scheme;
-        proxy_set_header Upgrade \\\$http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         
         # Increase timeouts for long-running RAG tasks
